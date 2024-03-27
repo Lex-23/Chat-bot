153d8a4020ebc5fc4375e9048100f4a847f644d0
Aliaksei <aliaksei.valenta.2022@gmail.com>
2024-03-27 17:29:53 +0400

    Create main models (#2)
    * create main models
    
    * fix structure
    
    * import all models to db
    
    * fix models relations
    
    * add model Profile, update method for getting context of conversation
    
    * delete utilities

--- app/models.py
+++ app/models.py
@@ -1,21 +1,94 @@
-from beanie import Document
+from __future__ import annotations
+from beanie import Document, Link, BackLink
 import datetime
 from pydantic import Field
-from typing import Optional, Union
+from typing import Optional, Union, List
+
 
 class User(Document):
     username: str = Field(max_length=100)
     created_at: datetime.date = datetime.date.today()
-    is_active: bool = True
-    chats: Optional[Union[list, None]]
+    chats: Optional[Union[List[Link[Chat]], None]] = None
 
     class Settings:
         name = "users_database"
 
     class Config:
-        cshema_extra = {
+        json_schema_extra = {
             "username": "John",
             "created_at": datetime.date.today(),
             "is_active": True,
             "chats": []
         }
+
+class Profile(Document):
+    user: Link[User]
+    email: str
+    age: int
+
+    class Settings:
+        name = "profiles_database"
+
+
+
+class Chat(Document):
+    name: str = Field(max_length=200)
+    type: str = Field(max_length=200)
+    owner_username: str = Field(max_length=100)
+    owner: BackLink[User] = Field(original_field="chats")
+    messages: Optional[Union[List[Link[Message]], None]]
+    created_at: datetime.date = datetime.datetime.now()
+    updated_at: datetime.date = datetime.datetime.now()
+    is_active: bool = True
+    type: str
+    last_message: Link[Message]
+
+
+class ChatBot(Chat):
+    purpose: str
+    description: str = Field(max_length=1000)
+    prescription: str = Field(max_length=1000)
+    communicative_style: str = Field(max_length=500)
+
+    class Settings:
+        name = "chatbots_database"
+
+    def generate_conversation_summary(self, limit: int = 10):
+        messages = self.messages[-limit:]
+        signed_messages = [f"{msg.username} said: {msg}" for msg in messages]
+        return ', '.join(signed_messages)
+
+    def get_context(self) -> str:
+        profile = Profile.get(user = self.owner)
+        context = f"""
+            Summary of conversation: {', '.join(self.generate_conversation_summary())}
+            User profile:
+            Username: {self.owner.username}
+            Email: {profile.email}
+            Age: {profile.age} 
+        """
+        return context
+
+    def generate_qa_template(self, question: str) -> str:
+
+        template = f"""
+            {self.purpose}/n
+            {self.description}/n
+            {self.prescription}/n
+            {self.get_context()}/n
+            User Query: {question}/n
+            {self.name}AI Advice:
+        """
+        return template
+    
+
+class Message(Document):
+    user: Link[User]
+    username: str
+    text: str
+    chat: BackLink[Chat] = Field(original_field="messages")
+    type: str = Field(max_length=200)
+    created_at: datetime.date = datetime.datetime.now()
+
+    class Settings:
+        name = "messages_database"

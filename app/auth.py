949e57a26cc9fec4db745e5baf94fdf8c6dbd078
Aliaksei <aliaksei.valenta.2022@gmail.com>
2024-03-28 15:56:18 +0400

    create simple Basic Auth

---
+++ app/auth.py
@@ -0,0 +1,28 @@
+from models import User
+
+from starlette.authentication import (
+    AuthCredentials, AuthenticationBackend, AuthenticationError
+)
+import base64
+import binascii
+
+class BasicAuthBackend(AuthenticationBackend):
+    async def authenticate(self, conn):
+        if "Authorization" not in conn.headers:
+            return
+
+        auth = conn.headers["Authorization"]
+        try:
+            scheme, credentials = auth.split()
+            if scheme.lower() != 'basic':
+                return
+            decoded = base64.b64decode(credentials).decode("ascii")
+        except (ValueError, UnicodeDecodeError, binascii.Error) as exc:
+            raise AuthenticationError('Invalid basic auth credentials')
+
+        username, _, password = decoded.partition(":")
+        if user := await User.find_one(User.username == username):
+            return AuthCredentials(["authenticated"]), user
+        else:
+            raise AuthenticationError(f'User <{username}> does not exists')
+        
--- app/main.py
+++ app/main.py
@@ -1,16 +1,20 @@
 from contextlib import asynccontextmanager
+from starlette.middleware import Middleware
+from starlette.middleware.authentication import AuthenticationMiddleware
 
 from fastapi import FastAPI
 from routes import router
 from db import init_db
 
+from auth import BasicAuthBackend
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await init_db()
     yield
 
 app = FastAPI(
-    lifespan=lifespan
+    lifespan=lifespan, 
+    middleware=[Middleware(AuthenticationMiddleware, backend=BasicAuthBackend())]
     )
 
 

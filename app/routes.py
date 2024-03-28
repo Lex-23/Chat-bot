73ef5d3de19182a7ec89cdc8451cfb3145d01878
Aliaksei <aliaksei.valenta.2022@gmail.com>
2024-03-28 16:03:15 +0400

    Create simple Basic Auth (#3)
    * create simple Basic Auth

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends, Request
 from models import User
 from typing import List
 from beanie import PydanticObjectId
 
+
 router = APIRouter()
+
+@router.get("/users/me")
+def read_current_user(request: Request):
+    return {"username": request.user.username}
 
 @router.get('/users', status_code=200)
 async def list_users() -> List[User]:
@@ -13,14 +18,19 @@ async def list_users() -> List[User]:
 
 @router.post('/users', status_code=201)
 async def create_user(user: User) -> dict:
-    await user.create()
-
-    return {"message": "User has been saved"} 
+    if not User.find_one(User.username == user.username):
+        await user.create()
+        return {"message": f"User <{user.username}> has been saved"}
+    else:
+        raise HTTPException(
+            status_code=400,
+            detail=f"User with username <{user.username}> already exists"
+        )
+ 
 
 @router.get('/users/{user_id}', status_code=200)
 async def retreive_user(user_id: PydanticObjectId) -> User:
     user = await User.get(user_id)
-
     return user
 
 @router.put('/users/{user_id}', status_code=200)
@@ -31,11 +41,15 @@ async def update_user(user: User, user_id: PydanticObjectId) -> User:
             status_code=404,
             detail="Resource has not found"
         )
-    user_to_update.username = user.username
-    user_to_update.chats = user.chats
-    await user_to_update.save()
-
-    return user_to_update
+    await user_to_update.set(
+        {
+            User.username: user.username,
+            User.chats: user.chats
+        }
+    )
+    
+    updated_user = await user_to_update.sync()
+    return updated_user
 
 @router.delete('/users/{user_id}', status_code=204)
 async def delete_user(user_id: PydanticObjectId):
@@ -47,4 +61,4 @@ async def delete_user(user_id: PydanticObjectId):
         )
     await user_to_delete.delete()
 
-    return {"message": f"User {user_to_delete.username} was deleted"}
+    return {"message": f"User <{user_to_delete.username}> was deleted succesfully"}
